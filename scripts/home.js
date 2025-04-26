d28930dab7bfa856e48b265fd8fe7471f5a6efc9
Miguel Angel Amengual <amengualma6@gmail.com>
2025-04-26 16:10:22 -0300

    Se arregla problema con el index.html y el home.js

---
+++ scripts/home.js
@@ -0,0 +1,45 @@
+let currentSlideIndex = 0;
+
+setTimeout(() => {
+    console.log('Iniciando');
+    autoIncrementSlide();
+    setInterval(() => {
+        autoIncrementSlide();
+    }, 5000);
+}, 10)
+
+function autoIncrementSlide() {
+    goToSlide(currentSlideIndex);
+    currentSlideIndex++;
+}
+
+function prevSlide() {
+    currentSlideIndex--;
+    goToSlide(currentSlideIndex--);
+}
+
+function nextSlide() {
+    currentSlideIndex++;
+    goToSlide(currentSlideIndex++);
+}
+
+function goToSlide(index) {
+    let slides = Array.from(document.getElementsByClassName("main-slideshow"));
+    let dots = Array.from(document.getElementsByClassName("dot-selector-slideshow"));
+
+    if (index > slides.length - 1) {
+        currentSlideIndex = 0;
+    } else if (index < 0) {
+        currentSlideIndex = slides.length - 1;
+    } else {
+        currentSlideIndex = index;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[currentSlideIndex].style.display = "block";
+    dots[currentSlideIndex].className += " active";
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,19 @@
-let currentSlideIndex = 0;
-
-setTimeout(() => {
-    autoIncrementSlide();
-    setInterval(() => {
-        autoIncrementSlide();
-    }, 5000);
-}, 1)
-
-function autoIncrementSlide() {
-    goToSlide(currentSlideIndex);
-    currentSlideIndex++;
-}
-
-function prevSlide() {
-    currentSlideIndex--;
-    goToSlide(currentSlideIndex--);
-}
-
-function nextSlide() {
-    currentSlideIndex++;
-    goToSlide(currentSlideIndex++);
-}
-
-function goToSlide(index) {
-    let slides = Array.from(document.getElementsByClassName("main-slideshow"));
-    let dots = Array.from(document.getElementsByClassName("dot-selector-slideshow"));
-
-    if (index > slides.length - 1) {
-        currentSlideIndex = 0;
-    } else if (index < 0) {
-        currentSlideIndex = slides.length - 1;
-    } else {
-        currentSlideIndex = index;
-    }
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[currentSlideIndex].style.display = "block";
-    dots[currentSlideIndex].className += " active";
-}
 
 fetch('./pages/adopta.html')
     .then(response => response.text())
     .then(html => {
         document.getElementById('adopt').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Error cargando el archivo:', error);
+    });
+
+
+
+fetch('./pages/home.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('home').innerHTML = html;
     })
     .catch(error => {
         console.error('Error cargando el archivo:', error);
