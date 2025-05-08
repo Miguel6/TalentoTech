7c28a8cae69bc2716cd20e244cee2628a828001c
Miguel Angel Amengual <amengualma6@gmail.com>
2025-05-08 19:21:04 -0300

    Se arregla el problema de los ruteos y se mejoran estilos

--- scripts/adopt.js
+++ scripts/adopt.js
@@ -1,26 +1,28 @@
-fetch('./../jsons/bubble-adopt-pets-response.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("No se pudo cargar el archivo JSON");
-        }
-        return response.json();
-    })
-    .then(pets => {
-        const container = document.getElementById("pet-container");
+setTimeout(() => {
+    fetch('./../jsons/bubble-adopt-pets-response.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("No se pudo cargar el archivo JSON");
+            }
+            return response.json();
+        })
+        .then(pets => {
+            const container = document.getElementById("pet-container");
 
-        pets.forEach(pet => {
-            const card = document.createElement("span");
-            card.className = "pet-card";
-            card.innerHTML = `
+            pets.forEach(pet => {
+                const card = document.createElement("span");
+                card.className = "pet-card";
+                card.innerHTML = `
                 <span class="sphere-image-wrapper">
                   <img src="${pet.image}" alt="Mascota ${pet.name}">
                 </span>
                 <h2 class="pet-name">${pet.name}</h2>
                 <h3>${pet.description}</h3>
-                <a href="#" class="adopt-button">Conocelo</a>`;
-            container.appendChild(card);
+                <button class="generic-button meet-him-button">Conocelo</a>`;
+                container.appendChild(card);
+            });
+        })
+        .catch(error => {
+            console.error("Error al cargar las mascotas:", error);
         });
-    })
-    .catch(error => {
-        console.error("Error al cargar las mascotas:", error);
-    });
+}, 100)
